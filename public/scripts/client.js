1867cbe3fc7d0971dd003dfb9ebe49db95cf8ca3
satya sindhu <movva001@gmail.com>
2021-12-15 16:51:16 -0500

    Dynamic Tweets with empty container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,55 +3,81 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+     $(document).ready(function() {
 
-$(() => {
 
-    const loadTweetsURL = "http://localhost:8080/tweets";
-    /**
-     * Entered tweet is converted to HTML and returned.
-     * @param {String} str
-     * @returns HTML
-     */
-    const escape = function(str) {
-      let div = document.createElement("div");
-      div.appendChild(document.createTextNode(str));
-      return div.innerHTML;
-    };
-  
-    /**
-     * THis method takes tweet object and returns String literal by rendering tweet element into HTML.
-     * @param {Object} data
-     * @returns
-     */
-    const createTweetElement = function(data) {
-      const tweet = `
-    <article class="article">
-    <header class="tweet-header">
-      <div class="tweet-profile-pic">
-        <img src="${data.user.avatars}">
-        <span>${data.user.name}</span>
-      </div>
-      <div class="watemark">
-        ${data.user.handle}
-      </div>
-    </header>
-      <div>
-      <p>${escape(data.content.text)}</p>
-      </div>
-      <div class="divider">
-      </div>
-    <footer class="tweet-footer">
-      <div>
-        ${timeago.format(data.created_at)}
-      </div>
-      <div>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
-  </article>`;
-  
-      return tweet;
-    };
+      const tweetData = {
+        "user": {
+          "name": "Newton",
+          "avatars": "https://i.imgur.com/73hZDYK.png",
+            "handle": "@SirIsaac"
+          },
+        "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+        "created_at": 1461116232227
+     }
+    
+      const data = [
+        {
+          "user": {
+            "name": "Newton",
+            "avatars": "https://i.imgur.com/73hZDYK.png"
+            ,
+            "handle": "@SirIsaac"
+          },
+          "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+          },
+          "created_at": 1461116232227
+        },
+        {
+          "user": {
+            "name": "Descartes",
+            "avatars": "https://i.imgur.com/nlhLi3I.png",
+            "handle": "@rd" },
+          "content": {
+            "text": "Je pense , donc je suis"
+          },
+          "created_at": 1461113959088
+        }
+      ]
 
+      //For each tweet object, render and prepend tweet element
+      const renderTweets = function(tweets) {         
+        for (const tweet of tweets) {
+          console.log(tweet)
+          const newTweet = createTweetElement(tweet);
+          $('#tweet-container').prepend(newTweet);
+        }
+      }
+       //Create dynamic tweet element using DB
+      const createTweetElement = function(tweets) {
+        console.log(tweets);
+        let $tweet = $(`<article class="tweet">
+        <header >
+          <div class="imgandname"> 
+            <img src="${tweetData.user.avatars}"/>
+            <span class="username">${tweetData.user.name}</span>
+          </div>
+          <span class="userhandle">${tweetData.user.handle}</span>
+        </header>
+        <h3>${tweetData.content.text}</h3>
+        <footer>
+          <h5>${timeago.format(tweetData.created_at)} </h5>
+          <div class="icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>`)
+      // let tweetElement = $tweet.append('#tweet-container');
+      // return tweetElement;
+      return $tweet
+      }
+      renderTweets(data);
+    });
+    
+
+      
